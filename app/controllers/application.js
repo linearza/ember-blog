f500a10d7079f9d92b9f118a52016a1b84241592
Michiel <linearza@users.noreply.github.com>
2017-06-04 17:30:38 +0200

    Adds ability to save custom posts with a title and body. Adds ability to delete posts. Adds some basic styling

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+
+  newPostTitle: null,
+  newPostBody: null,
+
   actions: {
     createPost() {
       this.get('store').createRecord('post', {
@@ -9,6 +13,22 @@ export default Ember.Controller.extend({
         body: 'No body'
       }).save().catch(e => {
         console.log(e.errors);
+      });
+    },
+
+    savePost() {
+      this.get('store').createRecord('post', {
+        title: this.get('newPostTitle'),
+        body: this.get('newPostBody'),
+      }).save().catch(e => {
+        console.log(e.errors);
+      });
+    },
+
+    deletePost(post) {
+      this.get('store').findRecord('post', post.id).then((item) => {
+        item.deleteRecord();
+        item.save();
       });
     }
   }
